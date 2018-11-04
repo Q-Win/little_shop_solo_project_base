8bcae57b6b56e73856050aea50be4efd84668ed4
Quinn Krug <quinn.krug@gmail.com>
2018-11-04 12:06:50 -0700

    updated models for discount

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
+  has_many :discounts
 
   validates_presence_of :name, :description
   validates :price, presence: true, numericality: {
-    only_integer: false, 
+    only_integer: false,
     greater_than_or_equal_to: 0
   }
   validates :inventory, presence: true, numericality: {
-    only_integer: true, 
+    only_integer: true,
     greater_than_or_equal_to: 0
   }
 
