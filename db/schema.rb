de7b6ce0068d8deb3d3a84719a1ed43ed1f2331a
Quinn Krug <quinn.krug@gmail.com>
2018-11-04 12:02:51 -0700

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181026014217) do
+ActiveRecord::Schema.define(version: 20181104190210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discounts", force: :cascade do |t|
+    t.integer "rate"
+    t.integer "quantity"
+  end
 
   create_table "items", force: :cascade do |t|
     t.bigint "user_id"
@@ -25,6 +30,8 @@ ActiveRecord::Schema.define(version: 20181026014217) do
     t.boolean "active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "discount_id"
+    t.index ["discount_id"], name: "index_items_on_discount_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -62,6 +69,7 @@ ActiveRecord::Schema.define(version: 20181026014217) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "discounts"
   add_foreign_key "items", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
